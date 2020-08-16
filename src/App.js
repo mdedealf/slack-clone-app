e38db250c51355fc3193a2edd95c3986561aaeb3
Mdedealf <dede.alfaruqq@gmail.com>
2020-08-17 00:33:07 +0700

    adding Header.js component and Header.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <div className="app">
+        {/* Header */}
+        <Header />
+        
+        {/* Sidebar */}
+        {/* React-Router */}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAPCj5TZZGipdVoEuRQOfALxK4grGPZgcU",
+    authDomain: "slack-clone-6250c.firebaseapp.com",
+    databaseURL: "https://slack-clone-6250c.firebaseio.com",
+    projectId: "slack-clone-6250c",
+    storageBucket: "slack-clone-6250c.appspot.com",
+    messagingSenderId: "875976916757",
+    appId: "1:875976916757:web:a2f66429bba9338a7ef31c",
+    measurementId: "G-SYCSBP9RVF"
+  };
