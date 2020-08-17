654e55014e41a63298a42130611385688c5c9896
Mdedealf <dede.alfaruqq@gmail.com>
2020-08-17 20:08:32 +0700

    add new header and sidebar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
-import Header from './Header'
+import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
-  return (
-    <div className="app">
-        {/* Header */}
-        <Header />
-        
-        {/* Sidebar */}
-        {/* React-Router */}
-    </div>
-  );
+	return (
+		<div className="app">
+			{/* Header */}
+			<Header />
+
+			<div className="app__body">
+				{/* Sidebar */}
+				<Sidebar />
+				{/* React-Router */}
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './Header.css';
+import { Avatar } from '@material-ui/core'
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 function Header() {
 	return (
 		<div className="header">
-			<h1>Header</h1>
+			<div className="header__left">
+				<Avatar 
+					className="header__avatar"
+					alt="Muhammad Dede Alfaruq"
+					src=""
+				/>
+				<AccessTimeIcon />
+            </div>
+			<div className="header__search">	
+				<SearchIcon />
+				<input placeholder="Search.." type="text"/>
+			</div>
+			<div className="header__right">
+				<HelpOutlineIcon />
+			</div>
 		</div>
 	);
 }
